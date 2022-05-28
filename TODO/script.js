f66786bc191e978ac1a51d9b6620fb6fe0a44343
S.Aviles <sammav96@gmail.com>
2022-05-28 15:33:25 -0400

    list area loaded
    Getting closer and closer to saving and loading the text area input into specified section. Next steps: keep testing different memoryDetails[i] placements/configurations

--- TODO/script.js
+++ TODO/script.js
@@ -4,28 +4,35 @@ const inputButton = document.querySelector('.add');
 const inputList = document.querySelector('.list');
 
 //This is where I start to advise JS what item will have interactivity
+document.addEventListener('DOMContentLoaded', loadList);
 inputButton.addEventListener('click', createList);
 inputList.addEventListener('click', deleteRow);
 inputList.addEventListener('click', completeRow);
 inputList.addEventListener('click', details);
 
+
 function createList(event){
     event.preventDefault();
-
     if (inputText.value == ''){
         return false;
     }
 
+    const listContainer = document.createElement('div');
+    listContainer.classList.add('listContainer');
+    inputList.appendChild(listContainer);
+    
+
     const listDiv = document.createElement('div');
     listDiv.classList.add('listDiv');
-    inputList.appendChild(listDiv);
-
+    listContainer.appendChild(listDiv);
+    
     const inputItem = document.createElement('li');
-    inputItem.innerHTML = inputText.value;
+    inputItem.innerText = inputText.value;
+    saveList(inputItem.innerText);
     inputItem.classList.add('list-text');
-    listDiv.appendChild(inputItem);
-    inputText.value = '';
-
+    listDiv.appendChild(inputItem); 
+    
+    
     const detailsButton = document.createElement('button');
     detailsButton.innerHTML = '<i class="fa-solid fa-caret-down" id="details"></i>';
     detailsButton.classList.add('listButton')
@@ -44,12 +51,15 @@ function createList(event){
     removeButton.classList.add('remove');
     listDiv.appendChild(removeButton);
 
+  
+    
     /*Below I create the drop down div for the details section. Including it here ensures it only happens once per new input */
-    const detailsDiv = document.createElement('div');
-    detailsDiv.classList.add('detailsDiv');
-    detailsDiv.style.display = 'none';
-    inputList.appendChild(detailsDiv);
-
+    const detailsForm = document.createElement('form');
+    detailsForm.classList.add('detailsForm');
+    detailsForm.style.display = 'none';
+    listContainer.appendChild(detailsForm);
+    
+    
     const why = document.createElement('div');
     why.classList.add('why-div');
     const whyPrompt = document.createElement('p');
@@ -66,22 +76,252 @@ function createList(event){
     whyAdd.innerHTML = '<i class="fa-thin fa-plus" id="whyAdd">';
     whyInput.appendChild(whyAdd);
 
+    for(var i = 0; i <= inputList.childNodes.length; i++){
+        whyText.setAttribute('id', 'test' + i);
+    
+
     whyAdd.addEventListener('click', setReason);
 
     function setReason(event){
-        event.preventDefault(); 
+        let addButton = event.target;
+        let parent = addButton.closest('div');
+        let textField = parent.firstChild;
         
-        if (whyText.value == ''){
+        if (textField.value == ''){
             return false;
         }
-
-        else if (whyAdd.classList[1] != 'active'){
-        whyInput.classList.toggle('setReasonDiv');
-        whyText.classList.toggle('setReason');
-        whyAdd.classList.toggle('active');
-        whyText.toggleAttribute('disabled');
+ 
+        else if (addButton.classList[1] != 'active'){
+        addButton.classList.toggle('active');
+        parent.classList.toggle('setReasonDiv');
+        textField.classList.toggle('setReason');
+        textField.toggleAttribute('disabled');
+         //find out how/where to properly save this info without duplicatng 
+         
         }
+        localStorage.setItem('memoryDetails'+[i], JSON.stringify(whyText.value)); //THIS CREATES A NEW KEY EVERY TIME I SAVE!!!!
     }
+    }
+   
+   
+
+    const miniGoals = document.createElement('div');
+    miniGoals.classList.add('miniGoals-div');
+    const miniPrompt = document.createElement('p');
+    miniPrompt.innerText = 'mini goals needed';
+    miniGoals.appendChild(miniPrompt);
+    const miniInput = document.createElement('div');
+    miniInput.classList.add('miniInput');
+    miniGoals.appendChild(miniInput);
+    const miniText = document.createElement('input');
+    miniText.type = 'text';
+    miniText.classList.add('miniText');
+    miniInput.appendChild(miniText);
+    const miniAdd = document.createElement('i');
+    miniAdd.innerHTML = '<i class="fa-thin fa-plus" id="miniAdd">';
+    miniInput.appendChild(miniAdd);
+    const miniListDiv = document.createElement('ul');
+    miniListDiv.classList.add('miniList');
+    miniGoals.appendChild(miniListDiv);
+
+    miniAdd.addEventListener('click', createMiniList);
+
+    function createMiniList(event){
+        event.preventDefault();
+        
+        if (miniText.value == ''){
+            return false;
+        } 
+
+        const miniItemsContainer = document.createElement('div')
+        miniItemsContainer.classList.add('miniItemsContainer')
+        miniListDiv.appendChild(miniItemsContainer);
+
+        const miniInputItem = document.createElement('li');
+        miniInputItem.innerHTML = miniText.value;
+        miniInputItem.classList.add('miniListText');
+        miniItemsContainer.appendChild(miniInputItem);
+        miniText.value = '';
+
+        const miniCheckButton = document.createElement('button');
+        miniCheckButton.innerHTML = '<i class="fa-solid fa-check" id="miniCheck"></i>';
+        miniCheckButton.classList.add('miniListButton');
+        miniCheckButton.classList.add('miniCheck');
+        miniItemsContainer.appendChild(miniCheckButton);
+
+        const miniRemoveButton = document.createElement('button');
+        miniRemoveButton.innerHTML = '<i class="fa-solid fa-minus" id="miniRemove"></i>';
+        miniRemoveButton.classList.add('miniListButton');
+        miniRemoveButton.classList.add('miniRemove');
+        miniItemsContainer.appendChild(miniRemoveButton);    
+}
+
+    miniGoals.addEventListener('click', completeMini);
+    miniGoals.addEventListener('click', removeMini);
+
+    const hDeadline = document.createElement('div');
+    hDeadline.classList.add('hDeadline-div');
+    const hDeadlinePrompt = document.createElement('p');
+    hDeadlinePrompt.innerText = 'Hopeful Deadline:';
+    hDeadline.appendChild(hDeadlinePrompt);
+    const hInput = document.createElement('input');
+    hInput.type = 'date';
+    hInput.classList.add('hopefulInput');
+    hDeadline.appendChild(hInput);
+
+    const rDeadline = document.createElement('div');
+    rDeadline.classList.add('rDeadline-div');
+    const rDeadlinePrompt = document.createElement('p');
+    rDeadlinePrompt.innerText = 'Realistic Deadline:';
+    rDeadline.appendChild(rDeadlinePrompt);
+    const rInput = document.createElement('input');
+    rInput.type = 'date';
+    rInput.max = "4000-12-31";
+    rInput.classList.add('realisticInput');
+    rDeadline.appendChild(rInput);
+
+    detailsForm.appendChild(why);
+    detailsForm.appendChild(miniGoals);
+    detailsForm.appendChild(hDeadline);
+    detailsForm.appendChild(rDeadline);
+
+    inputText.value = '';
+ //is this right? How do i get the info to restore on the page???
+}
+
+
+
+
+function saveList(x){
+
+    let memory;
+    if (localStorage.getItem('memory') === null){
+    memory = [];
+} else {
+    memory = JSON.parse(localStorage.getItem('memory'));
+}
+    memory.push(x);
+    localStorage.setItem('memory', JSON.stringify(memory));   
+}
+
+function saveDetails(x){
+
+    let memoryDetails;
+    
+    if (localStorage.getItem('memoryDetails') === null){
+    memoryDetails = [];
+} else {
+    memoryDetails = JSON.parse(localStorage.getItem('memoryDetails'));
+}
+    
+    memoryDetails.push(x);
+    localStorage.setItem('memoryDetails', JSON.stringify(memoryDetails));   
+}
+
+function loadList(){
+let memory;
+let memoryDetails;
+
+if (localStorage.getItem('memory') === null){
+    memory = [];
+} else {
+    memory = JSON.parse(localStorage.getItem('memory'));
+}
+
+
+memory.forEach(function(item){
+    
+    const listContainer = document.createElement('div');
+    listContainer.classList.add('listContainer');
+    inputList.appendChild(listContainer);
+    
+
+    const listDiv = document.createElement('div');
+    listDiv.classList.add('listDiv');
+    listContainer.appendChild(listDiv);
+    
+    const inputItem = document.createElement('li');
+    inputItem.innerText = item
+    inputItem.classList.add('list-text');
+    listDiv.appendChild(inputItem); 
+    
+    
+    const detailsButton = document.createElement('button');
+    detailsButton.innerHTML = '<i class="fa-solid fa-caret-down" id="details"></i>';
+    detailsButton.classList.add('listButton')
+    detailsButton.classList.add('closed');
+    listDiv.appendChild(detailsButton);
+
+    const checkButton = document.createElement('button');
+    checkButton.innerHTML = '<i class="fa-solid fa-check"id="check"></i>';
+    checkButton.classList.add('listButton');
+    checkButton.classList.add('check');
+    listDiv.appendChild(checkButton);
+
+    const removeButton = document.createElement('button');
+    removeButton.innerHTML = '<i class="fa-solid fa-minus" id="remove"></i>';
+    removeButton.classList.add('listButton');
+    removeButton.classList.add('remove');
+    listDiv.appendChild(removeButton);
+
+  
+    
+    /*Below I create the drop down div for the details section. Including it here ensures it only happens once per new input */
+    for(var i = 0; i <= inputList.childNodes.length; i++){//the 0 in the text field is coming from the var i = 0 here. I need to make the item before "forEach" equal to memoryDetails#, right now it's only equal to the [i] alone. Maybe add the get/set 4 line standard here
+    memoryDetails+[i].forEach(function(test){
+    const detailsForm = document.createElement('form');
+    detailsForm.classList.add('detailsForm');
+    detailsForm.style.display = 'none';
+    listContainer.appendChild(detailsForm);
+    
+    const why = document.createElement('div');
+    why.classList.add('why-div');
+    const whyPrompt = document.createElement('p');
+    whyPrompt.innerText = 'This goal is important to me because...';
+    why.appendChild(whyPrompt);
+    
+    const whyInput = document.createElement('div');
+    whyInput.classList.add('whyInput');
+    why.appendChild(whyInput);
+    
+    const whyText = document.createElement('textarea');
+    whyText.maxLength = '150';
+    whyText.classList.add('whyText');
+    whyText.value = test;
+    whyInput.appendChild(whyText);
+    const whyAdd = document.createElement('i');
+    whyAdd.innerHTML = '<i class="fa-thin fa-plus" id="whyAdd">';
+    whyInput.appendChild(whyAdd);
+    
+    
+    whyText.setAttribute('id', 'test' + i);
+    
+
+    whyAdd.addEventListener('click', setReason);
+
+    function setReason(event){
+        let addButton = event.target;
+        let parent = addButton.closest('div');
+        let textField = parent.firstChild;
+        
+        if (textField.value == ''){
+            return false;
+        }
+ 
+        else if (addButton.classList[1] != 'active'){
+        addButton.classList.toggle('active');
+        parent.classList.toggle('setReasonDiv');
+        textField.classList.toggle('setReason');
+        textField.toggleAttribute('disabled');
+         //find out how/where to properly save this info without duplicatng 
+         
+        }
+        
+
+        localStorage.setItem('memoryDetails'+[i], JSON.stringify(whyText.value)); //THIS CREATES A NEW KEY EVERY TIME I SAVE!!!!
+    }
+
+    
 
 
     const miniGoals = document.createElement('div');
@@ -159,22 +399,25 @@ function createList(event){
     rInput.classList.add('realisticInput');
     rDeadline.appendChild(rInput);
 
-    detailsDiv.appendChild(why);
-    detailsDiv.appendChild(miniGoals);
-    detailsDiv.appendChild(hDeadline);
-    detailsDiv.appendChild(rDeadline);
-
-    localStorage.setItem('list', '.list'); //is this right? How do i get the info to restore on the page???
+    detailsForm.appendChild(why);
+    detailsForm.appendChild(miniGoals);
+    detailsForm.appendChild(hDeadline);
+    detailsForm.appendChild(rDeadline);
+});
 }
+});
+} 
+
+
 
 function deleteRow(e){
     const removeButton = e.target;
 
     if (removeButton.classList[1] === 'remove'){
         const listItem = removeButton.closest('div');
-        const detailItem = listItem.nextElementSibling;        
-        listItem.remove();
-        detailItem.remove();
+        const listItemParent = listItem.parentNode;
+       
+        listItemParent.remove();
     }
 }
 
@@ -191,7 +434,6 @@ function completeRow(e){
 
 function details(e){
     const detailsButton = e.target;
-    const detailsDiv = document.querySelector('.detailsDiv');
 
     const listItem = detailsButton.closest('div');
     const connectedDiv = listItem.nextElementSibling;
@@ -219,7 +461,6 @@ function removeMini(e){
     }
 }
 
-
 function completeMini(e){
         const miniCompleteButton = e.target;
         if (miniCompleteButton.classList[1] === 'miniCheck'){
@@ -230,9 +471,8 @@ function completeMini(e){
         }
     }
 
-
-
-/* if i ever want to limit the mini add use:
-    const children = miniListDiv.children.length;
-    if (children == 3){
-    miniAdd.removeEventListener('click', createMiniList); */
+    /*whyText.innerText(test);
+        whyInput.classList.toggle('setReasonDiv');
+        whyText.classList.toggle('setReason');
+        whyAdd.classList.toggle('active');
+        whyText.toggleAttribute('disabled'); */
