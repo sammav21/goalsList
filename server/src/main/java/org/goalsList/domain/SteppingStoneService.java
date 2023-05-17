2e1857acfcf9db0b2c234ca2078ab0ee4eb0799c
sammav21 <sammav96@gmail.com>
2023-05-17 13:01:00 -0400

    steppingstone service layer done

---
+++ server/src/main/java/org/goalsList/domain/SteppingStoneService.java
@@ -0,0 +1,78 @@
+package org.goalsList.domain;
+
+import org.goalsList.data.SteppingStoneRepository;
+import org.goalsList.models.Goal;
+import org.goalsList.models.SteppingStone;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SteppingStoneService {
+
+    private SteppingStoneRepository repository;
+    public SteppingStoneService(SteppingStoneRepository repository){
+        this.repository = repository;
+    }
+
+    public SteppingStone findById(int steppingStoneId){
+        return repository.findById(steppingStoneId);
+    }
+    public List<SteppingStone> findByGoalId(int goalId){
+        return repository.findByGoalId(goalId);
+    }
+
+    public Result<SteppingStone> createSteppingStone(SteppingStone steppingStone){
+        Result<SteppingStone> result = validate(steppingStone);
+
+        if(!result.isSuccess()){
+            return result;
+        }
+        if(steppingStone.getSteppingStoneId() != 0){
+            result.setMessages("SteppingStone ID cannot be set before creating SteppingStone", ResultType.INVALID);
+            return result;
+        }
+
+        steppingStone = repository.createSteppingStone(steppingStone);
+        result.setPayload(steppingStone);
+        return result;
+    }
+
+    public Result<SteppingStone> updateSteppingStone(SteppingStone steppingStone){
+        Result<SteppingStone> result = validate(steppingStone);
+        if(!result.isSuccess()){
+            return result;
+        }
+        if(steppingStone.getSteppingStoneId() <= 0){
+            result.setMessages("SteppingStone ID required for update", ResultType.INVALID);
+            return result;
+        }
+        if(!repository.updateSteppingStone(steppingStone)){
+            String message = String.format("steppingStoneId: %s not found", steppingStone.getSteppingStoneId());
+            result.setMessages(message, ResultType.NOT_FOUND);
+        }
+        return result;
+    }
+
+    public Result<SteppingStone> deleteSteppingStone(int steppingStoneId){
+        Result<SteppingStone> result = new Result<>();
+        if(steppingStoneId <= 0){
+            result.setMessages("SteppingStone ID required for delete", ResultType.INVALID);
+            return result;
+        }
+        if(!repository.deleteSteppingStone(steppingStoneId)){
+            String message = String.format("steppingStoneId: %s not found", steppingStoneId);
+            result.setMessages(message, ResultType.NOT_FOUND);
+        }
+        return result;
+    }
+    private Result<SteppingStone> validate(SteppingStone steppingStone){
+        Result<SteppingStone> result = new Result<>();
+
+        if(steppingStone.getName() == null || steppingStone.getName().isEmpty() || steppingStone.getName().isBlank()){
+            result.setMessages("SteppingStone name required", ResultType.INVALID);
+            return result;
+        }
+        return result;
+    }
+}
