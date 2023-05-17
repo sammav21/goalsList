65942754daa429c652416a8b992f4f65f0506013
sammav21 <sammav96@gmail.com>
2023-05-17 16:49:03 -0400

    appUser Find/create implemented

---
+++ server/src/main/java/org/goalsList/data/AppUserRepository.java
@@ -0,0 +1,17 @@
+package org.goalsList.data;
+
+import org.goalsList.models.AppUser;
+import org.springframework.transaction.annotation.Transactional;
+
+public interface AppUserRepository {
+    @Transactional
+    AppUser findByUsername(String username);
+    @Transactional
+    AppUser findByUserId(int appUserId);
+    @Transactional
+    AppUser createUser(AppUser user);
+    @Transactional
+    void updateUser(AppUser user);
+    @Transactional
+    boolean deleteUser(int appUserId);
+}
