a750744cff52445ca6a74ad57d712229bb6b935e
sammav21 <sammav96@gmail.com>
2023-08-01 13:41:10 -0400

    Create Goal Component done. Folders/Layouts restructured

---
+++ client/src/layout/LoginPage.js
@@ -0,0 +1,10 @@
+import LoginCard from "../components/LoginCard";
+
+export default function LoginPage(props){
+
+    return(
+        <div className="loginScreen">
+            <LoginCard />
+        </div>
+    )
+}
--- client/src/layout/MainPage.js
+++ client/src/layout/MainPage.js
@@ -1,35 +1,30 @@
+import GoalCreate from "../components/GoalCreate"
 
-
-export default function Main(props){
+export default function MainPage(props){
     return(
     <>
         <header>
             <h1>Goals</h1>
         </header>
-        <form className="goalForm"> {/* component*/}
-            <div  className="input-section">
-                <input type="text" className="input" />
-                <button className="add"><i className="fa-thin fa-plus" id="addIcon"></i></button>
-            </div>
-        </form>
+        <GoalCreate refreshData={props.refreshData}/>
         <div className="list-section"> {/* component*/}
             <ul className="list">
                 {props.goals.map((goal) => {
                     {/* component*/}
                     return( 
-                    <div className="listContainer">
+                    <div className="listContainer" key={goal.goalId}>
                         <div className="listDiv">
                             <li className="list-text">{goal.name}</li>
-                            <button className="listButton closed"><i class="fa-solid fa-caret-down" id="details"></i></button>
-                            <button className="listButton check"><i class="fa-solid fa-check"id="check"></i></button>
-                            <button className="listButton remove"><i class="fa-solid fa-minus" id="remove"></i></button>
+                            <button className="listButton closed"><i className="fa-solid fa-caret-down" id="details"></i></button>
+                            <button className="listButton check"><i className="fa-solid fa-check"id="check"></i></button>
+                            <button className="listButton remove"><i className="fa-solid fa-minus" id="remove"></i></button>
                         </div>
                         <form className="detailsForm" >
                             <div className="why-div">
                                 <p>Why does this goal matter?</p>
                                 <div className="whyInput">
                                     <input type="textarea" className="whyText" value={goal.reason}></input>
-                                    <button><i class="fa-thin fa-plus" id="whyAdd"></i></button>
+                                    <button><i className="fa-thin fa-plus" id="whyAdd"></i></button>
                                 </div>
                             </div>
                             {/* stepping stone input section*/}
